e0caee66e6d46ff843914dcb973ae99553dbdd8b
srishti1102 <srishtiyadav30052004@gmail.com>
2024-11-06 21:57:59 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent=React.createElement("div", {id:"parent"},
+     React.createElement( "div",{id:"child"},
+       React.createElement("h1",{},"I am H! tag"),
+        React.createElement("h2",{},"I am H2 tag")
+    )
+);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
